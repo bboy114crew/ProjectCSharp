102fac0e88ed13e1aa958743ae6481c35f2493a8
DESKTOP-IA793PP\Jic <khanhlnse04056>
2017-03-23 01:28:32 +0700

    dsd

--- GameShop/GameShop/DAL/CategoryContext.cs
+++ GameShop/GameShop/DAL/CategoryContext.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +10,17 @@ namespace GameShop.DAL
 {
     class CategoryContext
     {
+        public DataTable getAll()
+        {
+            SqlConnection connection = DBContext.openConnection();
+            string sql = "SELECT [CategoryID], [CategoryName] FROM [Categories]"; 
+            SqlCommand com = new SqlCommand(sql, connection); 
+            com.CommandType = CommandType.Text;
+            SqlDataAdapter da = new SqlDataAdapter(com); 
+            DataTable dt = new DataTable(); 
+            da.Fill(dt);  
+            DBContext.closeConnection(connection);  
+            return dt;
+        }
     }
 }
