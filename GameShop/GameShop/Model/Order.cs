3849d7f125f64926e094886ff9b02e854bb95c25
DESKTOP-P0HKOGS\TD Chien <chientdse04142@fpt.edu.vn>
2017-03-23 02:19:03 +0700

    order

---
+++ GameShop/GameShop/Model/Order.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameShop.Model
+{
+    class Order
+    {
+        public int OrderID { get; set; }
+        public int CustomerID { get; set; }
+        List<OrderDetail> list;
+         public Order()
+        {
+            list = new List<OrderDetail>();
+        }
+        public Order(int OrderID,int CustomerID)
+        {
+            this.OrderID = OrderID;
+            this.CustomerID = CustomerID;
+        }
+        public List<OrderDetail> GetOrderList { get
+            { return list; }
+        }
+    }
+}
---
+++ GameShop/GameShop/Model/OrderDetail.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameShop.Model
+{
+    class OrderDetail
+    {
+        public int OrderID { get; set; }
+        public int ProductID { get; set; }
+        public int Quantity { get; set; }
+        public float UnitPrice { get; set; }
+        public OrderDetail()
+        {
+
+        }
+        public OrderDetail(int orderid,int proid,int quantity,float price)
+        {
+            this.OrderID = orderid;
+            this.ProductID = proid;
+            this.Quantity = quantity;
+            this.UnitPrice = price;
+        }
+    }
+}
